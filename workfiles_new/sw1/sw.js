0320c437bccb2ed8e51e711eb73f42bb2498495b
ShelbyResume <shelbyargabrite@gmail.com>
2026-04-28 12:05:35 -0700

    Add files via upload
    Homework 4 with correct paths.

---
+++ workfiles_new/sw1/sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'CacheA';
+const urlsToCache = [
+    '/',
+    '/workfiles_new/sw1/index.html',
+    '/workfiles_new/sw1/style.css',
+    '/workfiles_new/sw1/sw.js',
+    '/workfiles_new/sw1/lightblue.jpg',
+    '/workfiles_new/sw1/lightgold.jpg'
+];
+
+// Install event: Caches static assets
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch event: Intercepts network requests
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                // No cache hit - fetch from network
+                return fetch(event.request);
+            })
+    );
+});
+
+// Activate event: Cleans up old caches
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
